f898513d53818c4b41a3b886bc68d042d2f31617
Gab_km <the25thcromosome@gmail.com>
2014-07-01 07:06:01 +0900

    Modify which doesn't work

--- Profile/PowerShellEnvironment.ps1
+++ Profile/PowerShellEnvironment.ps1
@@ -1,4 +1,4 @@
-﻿$psdir="$(env:PSCustomScripts)"
-Get-ChildItem $psdir\autoload | where Extension -eq ".ps1" | %{.$_}
+﻿$psdir="${env:PSCustomScripts}"
+Get-ChildItem $psdir\autoload | where Extension -eq ".ps1" | %{.$_.FullName}
  
 Write-Host "Custom PowerShell Environment Loaded" -ForegroundColor Cyan
